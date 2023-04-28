d8ba9354de7599b0ff652a426eb76bfa765443ad
Duje Radicic <duje.radicic.00@fesb.hr>
2023-04-28 13:17:07 +0200

    add new data processing scripts

---
+++ etl.py
@@ -0,0 +1,86 @@
+from typing import Union, Dict
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import pandas as pd
+import numpy as np
+
+def extract_transfor_load(db_params: Dict[str, Union[str, int]],                        
+                          file_path: str,
+                          table_name: str,
+                          create_database: bool = False,
+                          new_database_name: Union[str, None] = None) -> None:
+
+    if create_database:
+        conn = psycopg2.connect(
+            database=db_params['db_name'],
+            user=db_params['db_user'],
+            password=db_params['db_password'],
+            host=db_params['db_host'],
+            port=db_params['db_port']
+        )
+
+        cur = conn.cursor()
+
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        cur.execute(f'DROP DATABASE IF EXISTS {new_database_name};')
+
+        cur.execute(f'CREATE DATABASE {new_database_name};')
+
+        cur.close()
+
+        conn.close()
+
+        db_params['db_name'] = new_database_name
+
+    conn = psycopg2.connect(
+        database=db_params['db_name'],
+        user=db_params['db_user'],
+        password=db_params['db_password'],
+        host=db_params['db_host'],
+        port=db_params['db_port']
+    )
+
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cur = conn.cursor()
+
+    df = pd.read_excel(file_path, sheet_name=0, header=5, usecols=lambda column: column.strip() not in ['', 'Unnamed: 4'])
+
+    df = df.iloc[:-1, 2:]
+
+    df = df.replace(np.nan, None)
+
+    columns = ["_" + "".join(s.strip().rstrip("%").split()) for s in df.columns.tolist()]
+
+    cur.execute(f'DROP TABLE IF EXISTS {table_name};')
+
+    cur.execute(f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        {columns[0]} VARCHAR(50) NOT NULL,
+        {columns[1]} VARCHAR(50),
+        {columns[2]} INT,
+        {columns[3]} INT,
+        {columns[4]} FLOAT,
+        {columns[5]} FLOAT,
+        {columns[6]} FLOAT,
+        {columns[7]} FLOAT,
+        {columns[8]} FLOAT,
+        {columns[9]} FLOAT,
+        {columns[10]} FLOAT,
+        {columns[11]} FLOAT
+        );
+    """)
+
+    values = df.values
+
+    columns_string = ', '.join(columns)
+
+    from psycopg2.extras import execute_values
+
+    execute_values(cur,
+                   f"INSERT INTO {table_name} ({columns_string}) VALUES %s;",
+                   values)
+
+    cur.close()
+    conn.close()
---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+from etl import extract_transfor_load
+
+load_dotenv()
+
+db_params = {
+    'db_name': os.getenv('DB_NAME'),
+    'db_user': os.getenv('DB_USER'),
+    'db_password': os.getenv('DB_PASSWORD'),
+    'db_host': os.getenv('DB_HOST'),
+    'db_port': os.getenv('DB_PORT'),
+}
+
+excel_file_path = './SpaceNK_2.0.xlsx'
+table_name = 'sales_data'
+new_db_name = 'sales_db_new'
+
+extract_transfor_load(
+    db_params,
+    file_path=excel_file_path,
+    table_name=table_name,
+    create_database=True,
+    new_database_name=new_db_name
+)
