b72c9d19c840ed2e3849ef6f8959d121d21e84ec
Duje Radicic <duje.radicic.00@fesb.hr>
2023-05-15 19:58:48 +0200

    add the black formatter and comments

--- etl.py
+++ etl.py
@@ -4,58 +4,79 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import pandas as pd
 import numpy as np
 
-def extract_transform_load(db_params: Dict[str, Union[str, int]],                        
-                          file_path: str,
-                          table_name: str,
-                          create_database: bool = False,
-                          new_database_name: Union[str, None] = None) -> None:
 
+def extract_transform_load(
+    db_params: Dict[str, Union[str, int]],
+    file_path: str,
+    table_name: str,
+    create_database: bool = False,
+    new_database_name: Union[str, None] = None,
+) -> None:
+    # Create a new database
     if create_database:
+        # Connect to the existing database
         conn = psycopg2.connect(
-            database=db_params['db_name'],
-            user=db_params['db_user'],
-            password=db_params['db_password'],
-            host=db_params['db_host'],
-            port=db_params['db_port']
+            database=db_params["db_name"],
+            user=db_params["db_user"],
+            password=db_params["db_password"],
+            host=db_params["db_host"],
+            port=db_params["db_port"],
         )
 
         cur = conn.cursor()
 
+        # Set the isolation level to autocommit to create a new database
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-        cur.execute(f'DROP DATABASE IF EXISTS {new_database_name};')
+        # Drop the new database if it already exists
+        cur.execute(f"DROP DATABASE IF EXISTS {new_database_name};")
 
-        cur.execute(f'CREATE DATABASE {new_database_name};')
+        # Create a new database
+        cur.execute(f"CREATE DATABASE {new_database_name};")
 
         cur.close()
 
         conn.close()
 
-        db_params['db_name'] = new_database_name
+        # Update the database name in the db_params dictionary
+        db_params["db_name"] = new_database_name
 
+    # Connect to the database
     conn = psycopg2.connect(
-        database=db_params['db_name'],
-        user=db_params['db_user'],
-        password=db_params['db_password'],
-        host=db_params['db_host'],
-        port=db_params['db_port']
+        database=db_params["db_name"],
+        user=db_params["db_user"],
+        password=db_params["db_password"],
+        host=db_params["db_host"],
+        port=db_params["db_port"],
     )
 
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     cur = conn.cursor()
 
-    df = pd.read_excel(file_path, sheet_name=0, header=5, usecols=lambda column: column.strip(' 0123456789') not in ['', 'Unnamed:'])
+    # Read data from an Excel file into a Pandas DataFrame
+    df = pd.read_excel(
+        file_path,
+        sheet_name=0,
+        header=5,
+        usecols=lambda column: column.strip(" 0123456789") not in ["", "Unnamed:"],
+    )
 
+    # Remove the last row from the DataFrame
     df = df.iloc[:-1]
 
+    # Replace NaN values with None
     df = df.replace(np.nan, None)
-    
+
+    # Generate column names by removing whitespace, numbers, and '%' symbol
     columns = ["_".join(s.strip().rstrip("%").split()) for s in df.columns.tolist()]
 
-    cur.execute(f'DROP TABLE IF EXISTS {table_name};')
+    # Drop the table if it already exists
+    cur.execute(f"DROP TABLE IF EXISTS {table_name};")
 
-    cur.execute(f"""
+    # Create a new table with specified columns
+    cur.execute(
+        f"""
     CREATE TABLE IF NOT EXISTS {table_name} (
         {columns[0]} VARCHAR(50) NOT NULL,
         {columns[1]} VARCHAR(50),
@@ -70,17 +91,21 @@ def extract_transform_load(db_params: Dict[str, Union[str, int]],
         {columns[10]} FLOAT,
         {columns[11]} FLOAT
         );
-    """)
+    """
+    )
 
+    # Convert the DataFrame values to a list of tuples
     values = df.values
 
-    columns_string = ', '.join(columns)
+    # Create a comma-separated string of column names
+    columns_string = ", ".join(columns)
 
+    # Insert the values into the table using execute_values for efficiency
     from psycopg2.extras import execute_values
 
-    execute_values(cur,
-                   f"INSERT INTO {table_name} ({columns_string}) VALUES %s;",
-                   values)
+    execute_values(
+        cur, f"INSERT INTO {table_name} ({columns_string}) VALUES %s;", values
+    )
 
     cur.close()
     conn.close()
--- main.py
+++ main.py
@@ -2,24 +2,32 @@ from dotenv import load_dotenv
 import os
 from etl import extract_transform_load
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Create a dictionary with database connection parameters
 db_params = {
-    'db_name': os.getenv('DB_NAME'),
-    'db_user': os.getenv('DB_USER'),
-    'db_password': os.getenv('DB_PASSWORD'),
-    'db_host': os.getenv('DB_HOST'),
-    'db_port': os.getenv('DB_PORT'),
+    "db_name": os.getenv("DB_NAME"),
+    "db_user": os.getenv("DB_USER"),
+    "db_password": os.getenv("DB_PASSWORD"),
+    "db_host": os.getenv("DB_HOST"),
+    "db_port": os.getenv("DB_PORT"),
 }
 
-excel_file_path = './SpaceNK_2.0.xlsx'
-table_name = 'sales_data'
-new_db_name = 'sales_db'
+# Specify the path to the Excel file
+excel_file_path = "./SpaceNK_2.0.xlsx"
 
+# Specify the name of the table in the database
+table_name = "sales_data"
+
+# Specify the name of the new database to be created
+new_db_name = "sales_db"
+
+# Call the extract_transform_load function to perform ETL process
 extract_transform_load(
     db_params,
     file_path=excel_file_path,
     table_name=table_name,
-    create_database=True,
-    new_database_name=new_db_name
+    create_database=True,  # Create a new database
+    new_database_name=new_db_name,
 )
